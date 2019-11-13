feb067452c9b9854803d3f1cc6f00e35eeeaba99
LeeYoungJin <mimiruru67@gmail.com>
2019-11-13 23:45:43 +0900

    병합 오류 수정

--- Project/SimpleGame/Item.cpp
+++ Project/SimpleGame/Item.cpp
@@ -15,8 +15,10 @@ void Item::applyEffect(Object* obj)
 	case 1:	// 배의 체력 증가
 		break;
 	case 2:	// 배의 속도 증가
+	{
 		value v = obj->getVelocity();
 		obj->setVelocity(1.5f * v.x, 1.5f * v.y, 1.5 * v.z);
+	}
 		break;
 	case 3:	// 배의 공격력 증가
 		break;
